585a0812ea9153552251d6445418963452dd89c2
vaishnavp1 <vaishnav.patil@kpit.com>
2022-09-09 12:36:47 +0530

    ch2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
