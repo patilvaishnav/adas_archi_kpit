14c8089dec7e4544c4101d5c3a8601cbfed5ac50
vaishnavp1 <vaishnav.patil@kpit.com>
2022-09-09 12:27:45 +0530

    ch2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { BrowserRouter,Router,Route,Link } from 'react-router-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
